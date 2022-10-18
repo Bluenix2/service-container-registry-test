241cbc9c37cb46178a96ff7894ca0d3ecbfeda70
Bluenix <bluenixdev@gmail.com>
2022-10-18 14:25:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import os
 
 print(os.environ)
 
-with open(os.environ['GITHUB_ENV'], 'a') as f:
-  f.write('EXECUTED_PYTHON=true')
+try:
+  with open(os.environ['GITHUB_ENV'], 'a') as f:
+    f.write('EXECUTED_PYTHON=true')
+except Exception as e:
+  print(e)
